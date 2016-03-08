e579bc76d5449b6829b5bf84bcb62a792e308784
Nathan Harrington <nharrington@wasatchphotonics.com>
2016-03-08 10:48:48 -0500

    slapchop integration into temperature logger

--- scripts/TemperatureLogger.py
+++ scripts/TemperatureLogger.py
@@ -5,14 +5,15 @@ cross platform wrapper from FastPM100 to collect power readings.
 Output:
     Create/Append to filename (default: combined_log.csv)
     timestamp: min,max,avg CCD Temp  min,max,avg Laser Temp,
-               min,max,avg PM100 Reading
+               min,max,avg PM100 Reading, min,max,avg yellow thermistor,
+               min,max,avg blue thermistor, min,max,avg amps
 
 
 
 Example configuration:
 
     Start the Thorlabs Optical power meter software
-    Set wavelength to 785
+    Set wavelength to 785, set high gain, no averaging
 
     cd BoardTester
     python setup.py develop
@@ -42,6 +43,8 @@ from fastpm100 import devices
 
 pm100usb = devices.ThorlabsMeter()
 
+slapchop = devices.SlapChopDevice()
+
 device = stroker_protocol.StrokerProtocolDevice()
 result = device.connect()
 serial = device.get_serial_number()
@@ -54,8 +57,8 @@ time.sleep(laser_enable_wait)
 filename = "combined_log.csv"
 log.info("Starting log of: %s to %s", serial, filename)
 
-period = 10
-samples = 1000
+period = 1
+samples = 100
 sleep_interval = (float(period) / float(samples))
 log.info("Logging %s samples every %s seconds (sample rate: %s)", \
          samples, period, sleep_interval)
@@ -68,11 +71,19 @@ l_temps = []
 c_temps = []
 l_power = []
 
+y_therm = []
+b_therm = []
+e_amper = []
+
 def write_data():
 
     l_temp_grp  = [min(l_temps), max(l_temps), numpy.average(l_temps)]
     c_temp_grp  = [min(c_temps), max(c_temps), numpy.average(c_temps)]
     l_power_grp = [min(l_power), max(l_power), numpy.average(l_power)]
+
+    y_therm_grp = [min(y_therm), max(y_therm), numpy.average(y_therm)]
+    b_therm_grp = [min(b_therm), max(b_therm), numpy.average(b_therm)]
+    e_amper_grp = [min(e_amper), max(e_amper), numpy.average(e_amper)]
 
     combined_data = c_temp_grp, l_temp_grp, l_power_grp
 
@@ -84,28 +95,55 @@ def write_data():
         ccd_str = ""
         for item in c_temp_grp:
             out_file.write("%s," % item)
-            ccd_str += "%2.2f," % item
+        ccd_str += "%2.2f," % item
 
         # Laser Temperature groups:
         las_str = ""
         for item in l_temp_grp:
             out_file.write("%s," % item)
-            las_str += "%2.2f," % item
+        las_str += "%2.2f," % item
 
         # Laser power groups:
         pow_str = ""
         for item in l_power_grp:
             out_file.write("%s," % item)
-            pow_str += "%2.2f," % item
+        pow_str += "%2.2f," % item
+
+        # Yellow thermistor group
+        yel_str = ""
+        for item in y_therm_grp:
+            out_file.write("%s," % item)
+        yel_str += "%2.2f," % item
+
+        # Blue thermistor group
+        blu_str = ""
+        for item in b_therm_grp:
+            out_file.write("%s," % item)
+        blu_str += "%2.2f," % item
+
+        # Electrical amperes group
+        amp_str = ""
+        for item in e_amper_grp:
+            out_file.write("%s," % item)
+        amp_str += "%2.2f," % item
+
 
         out_file.write("\n")
 
-        log.info("%s %s %s", ccd_str, las_str, pow_str)
+        log.info("%s %s %s %s %s %s", 
+                 ccd_str, las_str, pow_str,
+                 yel_str, blu_str, amp_str)
 
 def get_data():
     l_temps.append(device.get_laser_temperature())
     c_temps.append(device.get_ccd_temperature())
     l_power.append(pm100usb.read())
+
+    parts = slapchop.read()
+
+    y_therm.append(parts[0])
+    b_therm.append(parts[1])
+    e_amper.append(parts[2])
 
 def zmq_get_data():
     """ Like get data above, but also spew out on a zmq publisher
@@ -114,10 +152,18 @@ def zmq_get_data():
     l_temps.append(device.get_laser_temperature())
     c_temps.append(device.get_ccd_temperature())
     l_power.append(pm100usb.read())
+
+    parts = slapchop.read()
+    y_therm.append(parts[0])
+    b_therm.append(parts[1])
+    e_amper.append(parts[2])
        
     topic = "temperatures_and_power"
-    str_mesg = ("%s %s,%s,%s" \
-                % (topic, c_temps[-1], l_temps[-1], l_power[-1]))
+    str_mesg = ("%s %s,%s,%s,%s,%s,%s" \
+                % (topic, c_temps[-1], l_temps[-1], l_power[-1], \
+                          y_therm[-1], b_therm[-1], e_amper[-1]
+                  )
+               )
     socket.send(str_mesg)
 
 context = zmq.Context()
@@ -130,7 +176,11 @@ file_header = "Timestamp,CCD Min,CCD Max,CCD Average," \
               + "Laser Temperature Min,Laser Temperature Max," \
               + "Laser Temperature Average," \
               + "Laser Power Min,Laser Power Max," \
-              + "Laser Power Average,"
+              + "Laser Power Average,"\
+              + "Yellow thermistor min,Yellow Thermistor Max,Yellow Thermistor Average," \
+              + "Blue thermistor min,Blue Thermistor Max,Blue Thermistor Average," \
+              + "Amps Min,Amps Max,Amps Average,"
+
 # Print header if file does not exist
 if not os.path.exists(filename):
     with open(filename, "a") as out_file:
